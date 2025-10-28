f39e51c3cfda6ac3b057442c30761955d4985cbd
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2025-10-28 19:16:16 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import glob from 'glob'
 import injectHTML from 'vite-plugin-html-inject'
 import FullReload from 'vite-plugin-full-reload'
+import netlify from "@netlify/vite-plugin";
+
+// https://vite.dev/config/
+
 
 export default defineConfig({
   define: {
@@ -15,4 +19,6 @@ export default defineConfig({
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+   plugins: [netlify()],
 })
+
